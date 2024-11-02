1c51af90e0bf351dd3b371c3211419fc04b304fe
yayanyang <yayanyang@gmail.com>
2024-11-02 11:01:19 +0800

    init parserc

---
+++ crates/parserc/README.md
@@ -0,0 +1,3 @@
+# PARSERC
+
+`parserc` is an asynchronous compatible parser combinator library for rust language.
---
+++ crates/parserc/src/lib.rs
@@ -0,0 +1,3 @@
+//! `parsers` is a parser combinator library with a focus on safe paring, asynchronous compatible and large input data.
+
+pub mod inputs;
